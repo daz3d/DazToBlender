a691f57f92f9302ef72583d9ebc4bceb11b77667
samjay3d <71744685+samjay3d@users.noreply.github.com>
2021-03-01 10:54:06 -0800

    Removed Unused Material Code
    Renamed Clear Material to Reset Material.
    Fix Reset Material Back to default from import

--- Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/DtbMaterial.py
+++ Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/DtbMaterial.py
@@ -9,44 +9,6 @@ from . import Versions
 from . import MatDct
 from . import Util
 
-NGROUP3 = ['mcy_skin', 'mcy_eyewet', 'mcy_eyedry']
-SKIN = 0
-EWET = 1
-EDRY = 2
-
-mtable = [
-    ["Torso", 2],
-    ["Head", 2],
-    ["Body", 3],
-    ["Face", 1],
-    ["Lips", 1],
-    ["Teeth", 5],
-    ["Ears", 1],
-    ["Legs", 3],
-    ["EyeSocket", 1],
-    ["Mouth", 5],
-    ["Arms", 4],
-    ["Pupils", 7],
-    ["Fingernails", 4],
-    ["Cornea", 8],
-    ["Irises", 7],
-    ["Sclera", 7],
-    ["Toenails", 3],
-    ["EyeMoisture", 8],
-    ["EyeMoisture.00", 8],
-    ["Eyelashes", 0],
-    ["EylsMoisture", 8],
-    ["Genitalia", 2],
-    ["anus", 9],
-    ["labia", 9],
-    ["clitoris", 9],
-    ["vagina", 9],
-    ["glans", 9],
-    ["shaft", 9],
-    ["testicles", 9],
-    ["rectum", 9],
-]
-
 ftable = [
     ["d", "Diffuse"],
     ["b", "Bump"],
@@ -92,6 +54,40 @@ def getGroupNodeTree(key):
     if rtn is not None:
         return rtn.node_tree
 
+def default_material():
+    getGroupNodeTree("EyeDry")
+    getGroupNodeTree("EyeWet")
+    getGroupNodeTree("IrayUberSkin")
+
+def forbitMinus():
+    pbsdf = 'Principled BSDF'
+    for dobj in Util.myccobjs():
+        if dobj.type != 'MESH' or dobj == Global.getBody():
+            continue
+        for slot in dobj.material_slots:
+            mat = bpy.data.materials.get(slot.name)
+            if mat is None or mat.node_tree is None:
+                continue
+            mat_nodes = mat.node_tree.nodes
+            for mat_node in mat_nodes:
+                if pbsdf not in mat_node.name:
+                    continue
+                for node_input in mat_nodes[pbsdf].inputs:
+                    if len(node_input.links) != 0:
+                        continue
+                    if type(node_input.default_value) is float:
+                        if node_input.default_value < 0:
+                            node_input.default_value = 0.0
+                        if node_input.name == 'Metallic' and node_input.default_value == 1.0:
+                            node_input.default_value = 0.0
+                        if node_input.name == 'Specular' and node_input.default_value == 2.0:
+                            node_input.default_value = 0.2
+                    elif type(node_input.default_value) is list:
+                        for i in node_input.default_value:
+                            if type(i) is float:
+                                if node_input.default_value < 0:
+                                    node_input.default_value = 0.0
+
 
 def adjust_material(kind, inc_value, isEye):
     skincombi = [
@@ -125,10 +121,10 @@ def adjust_material(kind, inc_value, isEye):
     ]
     flg_skin = False
     if isEye:
-        tree = getGroupNodeTree(NGROUP3[EDRY])
+        tree = getGroupNodeTree("EyeDry")
         tbls = eyecombi
     else:
-        tree = getGroupNodeTree(NGROUP3[SKIN])
+        tree = getGroupNodeTree("IrayUberSkin")
         tbls = skincombi
         flg_skin = True
     if tree is None:
@@ -169,322 +165,6 @@ def getNidx(idx, nodes):
     return idx
 
 
-def toGroupInputsDefault(flg_eye):
-    k3 = [EDRY, EWET, SKIN]
-    for kidx, k in enumerate(k3):
-        dist_n = getGroupNode(NGROUP3[k])
-        for mat in bpy.data.materials:
-            if mat.node_tree is None:
-                continue
-            n = None
-            for sch_n in mat.node_tree.nodes:
-                if sch_n.name.startswith('Group') == False:
-                    continue
-                if sch_n.node_tree is None:
-                    continue
-                if sch_n.node_tree.name == dist_n.node_tree.name:
-                    n = sch_n
-                    break
-            if n is None:
-                continue
-            if flg_eye and ('eye' in sch_n.node_tree.name):
-                if ('dry' in sch_n.node_tree.name):
-                    for i, inp in enumerate(sch_n.inputs):
-                        if len(inp.links) > 0:
-                            continue
-                        if i == 2:
-                            inp.default_value = (0.5, 0.5, 1, 1)
-                        else:
-                            inp.default_value = (0.6, 0.6, 0.6, 1)
-                elif ('wet' in sch_n.node_tree.name):
-                    for i, inp in enumerate(sch_n.inputs):
-                        if len(inp.links) > 0:
-                            continue
-                        inp.default_value = (1.0, 1.0, 1.0, 1.0)
-            elif ('skin' in sch_n.node_tree.name):
-                for i, inp in enumerate(sch_n.inputs):
-                    if len(inp.links) > 0:
-                        continue
-                    if i == 4:
-                        inp.default_value = (0.5, 0.5, 1, 1)
-                    elif i < 6:
-                        inp.default_value = (0.6, 0.6, 0.6, 1)
-                    elif i == 6:
-                        inp.default_value = (0.287, 0.672, 0.565, 1)
-                    elif i == 7:
-                        #inp.default_value = (0.479, 0.012, 0.014, 1)
-                        inp.default_value = (0.479, 0.053, 0.065, 1)
-                    elif i == 8:
-                        inp.default_value = 0.7
-                    mname = mat.name.lower()
-                    if ('mouth' in mname) or ('teeth' in mname) or ('nail' in mname):
-                        if i == 1:
-                            inp.default_value = (0.9, 0.9, 0.9, 1)
-                        elif i == 2:
-                            inp.default_value = (0.2, 0.2, 0.2, 1)
-                        if ('teeth' in mname) or ('nail' in mname):
-                            if i == 6:
-                                if ('teeth' in mname):
-                                    inp.default_value = (0.45, 0.45, 0.45, 1)
-                                else:
-                                    inp.default_value = (0.22, 0.36, 0.5, 1)
-                            elif i == 7:
-                                if ('teeth' in mname):
-                                    inp.default_value = (0.45, 0.45, 0.45, 1)
-                                else:
-                                    inp.default_value = (0.5, 0.36, 0.22, 1)
-                            elif i == 8:
-                                inp.default_value = 0.0
-
-
-def toEyeDryDefault(ntree):
-    if ntree is None:
-        return
-    toGroupInputsDefault(True)
-    nodes = ntree.nodes
-    dvs = [
-        [1, 1, 0.0],
-        [1, 2, 0.0],
-        [2, 1, 0.08],
-        [2, 2, [Global.getSize()*0.006, Global.getSize()*0.003, Global.getSize()*0.003]],
-        [2, 5, 0.0],
-        [2, 6, 0.3],
-        [2, 7, 0.0],
-        [2, 'IOR', 1.35],
-        [2, 'Transmission', 0.0],
-        [2, 'Sheen', 0.0],
-        [6, 0, 0.5],
-        [6, 1, 1.0],
-        [6, 2, 1.0],
-        [3, 0, Global.getSize() * 0.001],  # NormalMap Strength
-        [4, 0, Global.getSize() * 0.001],  # Bump Strength
-        [4, 1, Global.getSize() * 0.001],  # Bump Distance
-    ]
-    for dv in dvs:
-        dv0 = getNidx(int(dv[0]), nodes)
-        if isinstance(dv[2], float) or isinstance(dv[2], int):
-            nodes[dv0].inputs[dv[1]].default_value = dv[2]
-        else:
-            for i in range(len(dv[2])):
-                nodes[dv[0]].inputs[dv[1]].default_value[i] = dv[2][i]
-    NodeArrange.toNodeArrange(ntree.nodes)
-
-
-def toEyeWetDefault(ntree):
-    if ntree is None:
-        return
-    toGroupInputsDefault(True)
-    nodes = ntree.nodes
-    dvs = [
-        [4, 0, 1.45],
-        [6, 0, [1.0, 1.0, 1.0, 1.0]],
-        [7, 1, 0.0],
-        [2, 0, 0.1],
-        [3, 0, 0.1],
-        [3, 1, 0.1],
-        [7, 0, [1.0, 1.0, 1.0, 1.0]],
-        [7, 1, 0.0],
-        [7, 4, 1.0],
-        [7, 5, 1.0],
-        [7, 7, 0.0],
-        [7, 'Transmission', 1.0],
-        [7, 'Alpha', 0.5],
-    ]
-    for dv in dvs:
-        dv0 = getNidx(int(dv[0]),  nodes)
-        if isinstance(dv[2], float) or isinstance(dv[2], int):
-            nodes[dv0].inputs[dv[1]].default_value = dv[2]
-        else:
-            for i in range(len(dv[2])):
-                nodes[dv0].inputs[dv[1]].default_value[i] = dv[2][i]
-    NodeArrange.toNodeArrange(ntree.nodes)
-
-
-def toSkinDefault(ntree):
-    if ntree is None:
-        return
-    toGroupInputsDefault(False)
-    nds = ntree.nodes
-    nds[getNidx(4, nds)].space = 'OBJECT'
-    nds[getNidx(13, nds)].falloff = 'RANDOM_WALK'
-    nds[getNidx(14, nds)].falloff = 'RANDOM_WALK'
-    dvs = [
-        [5, 0, Global.getSize()*0.01],  # NormalMap Strength
-        [6, 0, Global.getSize()*0.002],  # Bump Strength
-        [6, 1, Global.getSize()*0.002],  # Bump Distance
-        [7, 0, 1.330],  # Fresnel
-        [13, 1, Global.getSize()*0.005],  # BlueSSS
-        [14, 1, Global.getSize()*0.030],  # RedSSS
-        [8, 1, 0.0],
-        [9, 1, 0.1],
-        [10, 1, 0],
-        [8, 2, 0.0],
-        [9, 2, 0.0],
-        [10, 2, 0.0],
-        [2, 1, 0.1],
-        [2, 4, 0.0],
-        [2, 'IOR', 1.33],
-        [2, 'Transmission', 0.0],
-        [2, 'Alpha', 1.0],
-        [4, 2, Global.getSize() * 0.0012],  # Displacement Height
-        [4, 1, Global.getSize() * 0.005],  # Displacement Middle
-        [13, 4, 0.1],
-        [14, 4, 0.1],
-        [2, 14, 1.33],
-        [2, 2, [
-            0.04 * Global.getSize(),
-            0.008 * Global.getSize(),
-            0.002 * Global.getSize()
-        ]],
-        [11, 0, 0.5],
-        [11, 1, 1.0],
-        [11, 2, 1.0],
-        [7, 0, 1.33],
-    ]
-    for dv in dvs:
-        dv0 = getNidx(int(dv[0]), nds)
-        if isinstance(dv[2], float) or isinstance(dv[2], int):
-            nds[dv0].inputs[dv[1]].default_value = dv[2]
-        else:
-            for i in range(len(dv[2])):
-                nds[dv0].inputs[dv[1]].default_value[i] = dv[2][i]
-    NodeArrange.toNodeArrange(ntree.nodes)
-
-
-def clear_past_nodegroup():
-    for ng in NGROUP3:
-        for s in bpy.data.node_groups:
-            if s.name == ng:
-                bpy.data.node_groups.remove(s, do_unlink=True)
-                break
-
-
-def forbitMinus():
-    pbsdf = 'Principled BSDF'
-    for dobj in Util.myccobjs():
-        if dobj.type != 'MESH' or dobj == Global.getBody():
-            continue
-        for slot in dobj.material_slots:
-            mat = bpy.data.materials.get(slot.name)
-            if mat is None or mat.node_tree is None:
-                continue
-            mat_nodes = mat.node_tree.nodes
-            for mat_node in mat_nodes:
-                if pbsdf not in mat_node.name:
-                    continue
-                for node_input in mat_nodes[pbsdf].inputs:
-                    if len(node_input.links) != 0:
-                        continue
-                    if type(node_input.default_value) is float:
-                        if node_input.default_value < 0:
-                            node_input.default_value = 0.0
-                        if node_input.name == 'Metallic' and node_input.default_value == 1.0:
-                            node_input.default_value = 0.0
-                        if node_input.name == 'Specular' and node_input.default_value == 2.0:
-                            node_input.default_value = 0.2
-                    elif type(node_input.default_value) is list:
-                        for i in node_input.default_value:
-                            if type(i) is float:
-                                if node_input.default_value < 0:
-                                    node_input.default_value = 0.0
-
-
-def default_material():
-    toEyeDryDefault(getGroupNodeTree(ngroup3(EDRY)))
-    toEyeWetDefault(getGroupNodeTree(ngroup3(EWET)))
-    toSkinDefault(getGroupNodeTree(ngroup3(SKIN)))
-
-
-def skin_levl(flg_high):
-    for slot in Global.getBody().material_slots:
-        ROOT = bpy.data.materials[slot.name].node_tree.nodes
-        LINK = bpy.data.materials[slot.name].node_tree.links
-        SNBP = None
-        for n in ROOT:
-            if n.name.startswith('Group') == False:
-                continue
-            SNBP = n
-            break
-        if SNBP is None or SNBP.node_tree.name != ngroup3(SKIN):
-            continue
-        if ('Cycles' in SNBP.outputs) == False or ('EEVEE' in SNBP.outputs) == False:
-            continue
-        for n in ROOT:
-            if n.name.startswith('Material Output') and n.target == 'CYCLES':
-                if flg_high:
-                    LINK.new(SNBP.outputs['Cycles'], n.inputs['Surface'])
-                else:
-                    LINK.new(SNBP.outputs['EEVEE'], n.inputs['Surface'])
-    Global.setRenderSetting(flg_high)
-
-
-def readImages(dct):
-    for slot in Global.getBody().material_slots:
-        ROOT = bpy.data.materials[slot.name].node_tree.nodes
-        LINK = bpy.data.materials[slot.name].node_tree.links
-        SNBP = None
-        for n in ROOT:
-            if n.name.startswith('Group') == False:
-                continue
-            SNBP = n
-            break
-        if SNBP is None:
-            continue
-        for midx in range(len(mtable)):
-            mname = mtable[midx][0]
-            mban = mtable[midx][1]
-            if mban == 0 or mban == 8:
-                continue
-            if (mname.lower() in slot.name.lower()):
-                for fidx, ft in enumerate(ftable):
-                    key = str(mban) + ft[0]
-                    if (ft[1] not in SNBP.inputs):
-                        continue
-                    if key in dct.keys():
-                        adr = dct[key]
-                    else:
-                        adr = ""
-                    if os.path.exists(adr) == False:
-                        continue
-                    inp = SNBP.inputs[ft[1]]
-                    if inp.links is None or len(inp.links) == 0:
-                        SNTIMG = ROOT.new(type='ShaderNodeTexImage')
-                        LINK.new(SNTIMG.outputs[0], inp)
-                    for link in inp.links:
-                        if link.from_node.name.startswith("Image Texture"):
-                            SNTIMG = link.from_node
-                            img = bpy.data.images.load(filepath=adr)
-                            SNTIMG.image = img
-                            if fidx != 0:
-                                Versions.to_color_space_non(SNTIMG)
-
-
-def insert_bump_map(nodes, links):
-    rtn = [None, None]
-    PBSDF = None
-    NML = None
-    for n in nodes:
-        if n.name == 'Principled BSDF':
-            PBSDF = n
-        elif n.name == 'Normal Map':
-            NML = n
-    if PBSDF is None:
-        return nodes
-    nlinks = PBSDF.inputs['Normal'].links
-    for nl in nlinks:
-        BUMP = nodes.new(type='ShaderNodeBump')
-        if NML is not None and nl.from_node == NML:
-            links.new(BUMP.outputs['Normal'], PBSDF.inputs['Normal'])
-            links.new(NML.outputs['Normal'], BUMP.inputs['Normal'])
-            rtn[1] = BUMP
-        elif nl.from_node.name == 'Bump Map':
-            rtn[1] = nl.from_node
-    if rtn[1] is None:
-        BUMP = nodes.new(type='ShaderNodeBump')
-        links.new(BUMP.outputs['Normal'], PBSDF.inputs['Normal'])
-        rtn[1] = BUMP
-    rtn[0] = nodes
-    return rtn
 
 # endregion top-level methods
 
@@ -704,9 +384,9 @@ class DtbShaders:
                     if image_path != "":
                         md = MatDct.MatDct()
                         cary = md.cloth_dct_0(image_path)
-                        pandb = insert_bump_map(mat_nodes, mat_links)
-                        BUMP = pandb[1]
-                        mat_nodes = pandb[0]
+                        #pandb = insert_bump_map(mat_nodes, mat_links)
+                        #BUMP = pandb[1]
+                        #mat_nodes = pandb[0]
                         prin_bsdf_node.inputs['Specular'].default_value = 0.3
                         if(len(prin_bsdf_node.inputs['Alpha'].links) > 0):
                             Versions.eevee_alpha(mat, 'BLEND', 0)
