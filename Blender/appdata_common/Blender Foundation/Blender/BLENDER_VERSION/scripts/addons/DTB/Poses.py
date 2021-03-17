7c14ae4eeaa07a4f3f2ce1951f22fc857c6371f0
samjay3d <71744685+samjay3d@users.noreply.github.com>
2021-03-16 20:47:03 -0700

    Environment Update Fixed (For now)
    - Locked Locations at Export Fixed
    - Fixed Armature for Props/Environments
    - Combine Posing/Bone fixes for Figure and Environment. (Still Seprated for Figure and a lot of areas)
    - TO DO:
    Update Materials
    Delete Materials that duplicates.

--- Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/Poses.py
+++ Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/Poses.py
@@ -2,14 +2,261 @@ import bpy
 import os
 import math
 import json
+import mathutils
 from . import DataBase
 from . import Versions
 from . import Global
 import re
 
-
+#TODO: Figure out a way to use for Figure and Prop
 class Posing:
-    #TODO Refactor and update to Reenable
+    
+    bone_head_tail_dict = {}
+    bone_limits_dict = {}
+    pose_data_dict = {}
+    
+    def __init__(self, asset):
+        if asset == "FIG":
+            self.bone_limits = DataBase.get_bone_limits_dict()
+            self.get_pose_data(asset)
+
+        if asset == "ENV":
+            self.load_bone_limits(asset)
+            self.load_bone_head_tail_data(asset)
+            self.get_pose_data(asset)
+        
+    
+    def load_bone_head_tail_data(self, asset):
+        input_file = open(os.path.join(Global.getHomeTown(), asset + "_boneHeadTail.csv"), "r")
+        lines = input_file.readlines()
+        input_file.close()
+        self.bone_head_tail_dict = dict()
+        for line in lines:
+            line_split = line.split(",")
+            self.bone_head_tail_dict[line_split[0]] = line_split
+    
+
+    #Bone property
+    def get_bone_head_tail_data(self, bname):
+        bname = bname.split(".00")[0] # To deal with Duplicates
+        if self.bone_head_tail_dict is None:
+            self.get_bone_head_tail_data()
+        if bname in self.bone_head_tail_dict.keys():
+            return self.bone_head_tail_dict[bname]
+        return None
+    
+
+    def set_bone_head_tail(self, bone):
+        binfo = self.get_bone_head_tail_data(bone.name)
+        if binfo is None:
+            return False
+        else:
+            # set head
+            bone.head[0] = float(binfo[1])
+            bone.head[1] = -float(binfo[3])
+            bone.head[2] = float(binfo[2])
+            
+            # set tail
+            bone.tail[0] = float(binfo[4])
+            bone.tail[1] = -float(binfo[6])
+            bone.tail[2] = float(binfo[5])
+
+            # calculate roll aligning bone towards a vector
+            align_axis_vec = mathutils.Vector((
+                                float(binfo[7]),
+                                -float(binfo[9]),
+                                float(binfo[8])
+                                ))
+            bone.align_roll(align_axis_vec)
+            
+            return True
+
+
+    def load_bone_limits(self, asset):
+        input_file = open(os.path.join(Global.getHomeTown(), asset + "_boneLimits.csv"), "r")
+        lines = input_file.readlines()
+        input_file.close()
+
+        for line in lines:
+            line_split = line.split(',')
+            bone_limit = []
+            
+            bone_limit.append(line_split[0])
+            bone_limit.append(line_split[1])
+            bone_limit.append(float(line_split[2]))
+            bone_limit.append(float(line_split[3]))
+            bone_limit.append(float(line_split[4]))
+            bone_limit.append(float(line_split[5]))
+            bone_limit.append(float(line_split[6]))
+            bone_limit.append(float(line_split[7]))
+            self.bone_limits_dict[bone_limit[0]] = bone_limit
+    
+
+    def get_bone_limits_dict(self, bname):
+        bname = bname.split(".00")[0] # To deal with Duplicates
+        if len(self.bone_limits_dict.keys()) == 0:
+            self.load_bone_limits()
+        if bname in self.bone_limits_dict.keys():
+            return self.bone_limits_dict[bname]
+        
+    def bone_limit_modify(self, bone):
+        bone_limit = self.get_bone_limits_dict(bone.name)
+        order = bone_limit[1]
+        new_bone_limit = self.reorder_limits(order, bone_limit, bone.name)
+        new_order = self.get_rotation_order(order)
+        bone.rotation_mode = new_order
+        bone.constraints.new('LIMIT_ROTATION')
+        rot_limit = bone.constraints['Limit Rotation']
+        rot_limit.owner_space = 'LOCAL'
+        rot_limit.use_transform_limit = True
+        rot_limit.use_limit_x = True
+        rot_limit.min_x = math.radians(new_bone_limit[2])
+        rot_limit.max_x = math.radians(new_bone_limit[3])
+        rot_limit.use_limit_y = True
+        rot_limit.min_y = math.radians(new_bone_limit[4])
+        rot_limit.max_y = math.radians(new_bone_limit[5])
+        rot_limit.use_limit_z = True
+        rot_limit.min_z = math.radians(new_bone_limit[6])
+        rot_limit.max_z = math.radians(new_bone_limit[7])
+
+        bone.use_ik_limit_x = True
+        bone.use_ik_limit_y = True
+        bone.use_ik_limit_z = True
+        if 'shin' in bone.name.lower():
+            bone.ik_min_x = math.radians(1)
+            bone.use_ik_limit_x = False
+        else:
+            bone.ik_min_x = math.radians(new_bone_limit[2])
+        bone.ik_max_x = math.radians(new_bone_limit[3])
+        bone.ik_min_y = math.radians(new_bone_limit[4])
+        bone.ik_max_y = math.radians(new_bone_limit[5])
+        bone.ik_min_z = math.radians(new_bone_limit[6])
+        bone.ik_max_z = math.radians(bone_limit[7])
+
+        if bone.name[1:] == 'Shin' or 'Thigh' in bone.name:
+            bone.ik_stiffness_y = 0.99
+            bone.ik_stiffness_z = 0.99
+            if 'ThighTwist' in bone.name:
+                bone.ik_stiffness_x = 0.99
+
+    def reorder_limits(self, rotation_order, limits, name):
+        if rotation_order == 'XYZ':
+            # YZ switch (Y <-> Z)
+            temp1 = limits[4]
+            temp2 = limits[5]
+            limits[4] = limits[6]
+            limits[5] = limits[7]
+            limits[6] = temp1
+            limits[7] = temp2
+
+            # XY switch (X <-> Y)
+            temp1 = limits[2]
+            temp2 = limits[3]
+            limits[2] = limits[4]
+            limits[3] = limits[5]
+            limits[4] = temp1
+            limits[5] = temp2
+
+            if "right" in name.lower():
+                # Y invert (-Y)
+                limits[4] = -limits[4]
+                limits[5] = -limits[5]
+                # Z invert (-Z)
+                limits[6] = -limits[6]
+                limits[7] = -limits[7]
+
+        elif rotation_order == 'XZY':
+            # XY switch (X <-> Y)
+            temp1 = limits[2]
+            temp2 = limits[3]
+            limits[2] = limits[4]
+            limits[3] = limits[5]
+            limits[4] = temp1
+            limits[5] = temp2
+
+            # X invert (-X)
+            limits[2] = -limits[2]
+            limits[3] = -limits[3]
+
+            if "right" in name.lower():
+                # Y invert (-Y)
+                limits[4] = -limits[4]
+                limits[5] = -limits[5]
+                # Z invert (-Z)
+                limits[6] = -limits[6]
+                limits[7] = -limits[7]
+
+        elif rotation_order == "ZXY":
+            # XY switch (X <-> Y)
+            temp1 = limits[2]
+            temp2 = limits[3]
+            limits[2] = limits[4]
+            limits[3] = limits[5]
+            limits[4] = temp1
+            limits[5] = temp2
+
+            # YZ switch (Y <-> Z)
+            temp1 = limits[4]
+            temp2 = limits[5]
+            limits[4] = limits[6]
+            limits[5] = limits[7]
+            limits[6] = temp1
+            limits[7] = temp2
+
+        elif rotation_order == "ZYX":
+             # YZ switch (Y <-> Z)
+            temp1 = limits[4]
+            temp2 = limits[5]
+            limits[4] = limits[6]
+            limits[5] = limits[7]
+            limits[6] = temp1
+            limits[7] = temp2
+
+            # X invert (-X)
+            limits[2] = -limits[2]
+            limits[3] = -limits[3]
+
+        return limits
+
+    def get_pose_data(self, asset):
+        hometown = Global.getHomeTown()
+
+        padr = os.path.join(hometown, asset + ".transforms")
+        if os.path.exists(padr) == False:
+            return
+        with open(padr, errors='ignore', encoding='utf-8') as f:
+            data = json.load(f)
+
+        # Rename the root bone
+        for key in data:
+            if key.startswith('Genesis'):
+                new_key = 'root'
+                data[key]["Name"] = new_key
+                data[key]["Object Type"] = 'BONE'
+                data[new_key] = data.pop(key)
+
+        self.pose_data_dict = data
+
+
+    def get_objects_pose(self,obj):
+        obj_name = obj.name.replace(".Shape", "")
+        if obj_name in self.pose_data_dict:
+            return self.pose_data_dict[obj_name]
+
+        for key in self.pose_data_dict:
+            if obj_name == key:
+                return self.pose_data_dict[obj_name]
+            elif obj_name.replace("_dup_", " ") == key:
+                return self.pose_data_dict[key]
+            elif len(obj_name.split(".00")) > 1:
+                temp_name = obj_name.split(".00")[0] + " " + str(int(obj_name.split(".00")[1]) + 1)
+                if temp_name == key:
+                    return self.pose_data_dict[key] 
+            elif self.pose_data_dict[key]["Name"] == obj_name:
+                return self.pose_data_dict[key]
+        return {}
+
+
     def clear_pose(self):
         Versions.select(Global.getAmtr(), True)
         Versions.active_object(Global.getAmtr())
@@ -18,6 +265,7 @@ class Posing:
         bpy.ops.pose.transforms_clear()
         Global.setOpsMode('OBJECT')
 
+    #TODO Refactor and update to Reenable
     def pose_copy(self,dur):
         if os.path.exists(dur) == False:
             return
@@ -137,8 +385,10 @@ class Posing:
             return 'YZX'
 
     
-    def make_pose(self, transform_data):
-        bone_limits = DataBase.get_bone_limits_dict()
+    def make_pose(self):
+        bone_limits = self.bone_limits_dict
+        transform_data = self.pose_data_dict
+
         pbs = Global.getAmtr().pose.bones
         for bone_limit_key in bone_limits:
             bone_limit = bone_limits[bone_limit_key]
@@ -157,7 +407,7 @@ class Posing:
                     pbs[bname].location[0] = float(position[0])
                     # Y invert (-Y)
                     pbs[bname].location[1] = -float(position[1])
-                    # Y invert (-z)
+                    # Z invert (-Z)
                     pbs[bname].location[2] = -float(position[2])
                     
                     
@@ -176,24 +426,8 @@ class Posing:
                             action.name = Global.get_asset_name() + " Pose Library"
                     bpy.ops.pose.select_all(action="DESELECT")
 
-
+    
     def restore_pose(self):
         Versions.active_object(Global.getAmtr())
         Global.setOpsMode("POSE")
-        hometown = Global.getHomeTown()
-
-        padr = os.path.join(hometown, "FIG.transforms")
-        if os.path.exists(padr) == False:
-            return
-        with open(padr, errors='ignore', encoding='utf-8') as f:
-            data = json.load(f)
-
-        # Rename the root bone
-        for key in data:
-            if key.startswith('Genesis'):
-                new_key = 'root'
-                data[key]["Name"] = new_key
-                data[key]["Object Type"] = 'BONE'
-                data[new_key] = data.pop(key)
-
-        self.make_pose(data)
+        self.make_pose()
