5a0665ab016af5eec5d54947d41e9af97c5ef77f
samjay3d <71744685+samjay3d@users.noreply.github.com>
2021-06-07 10:55:58 -0700

    Change how the script check the version of the blender
    Fixes error where hotfixes of the Blender version exists.

--- Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/Versions.py
+++ Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/Versions.py
@@ -4,24 +4,10 @@ import math
 from . import Global
 from . import Util
 #BV
-if bpy.app.version == (2, 79, 0):
-    BV = 2.79
-elif bpy.app.version == (2, 80, 0):
-    BV = 2.80
-elif bpy.app.version == (2, 81, 0):
-    BV = 2.81
-elif bpy.app.version == (2, 82, 0):
-    BV = 2.82
-elif bpy.app.version == (2, 83, 0):
-    BV = 2.83
-elif bpy.app.version == (2, 90, 0):
-    BV = 2.90
-elif bpy.app.version == (2, 91, 0):
-    BV = 2.91
-elif bpy.app.version == (2, 92, 0):
-    BV = 2.92
-else:
-    BV = 2.93
+def get_version():
+    return bpy.app.version[0] + (bpy.app.version[1]/100)
+
+BV = get_version()
 
 my_local_language = 'en_US'
 
