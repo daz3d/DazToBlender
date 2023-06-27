8d6779a2c5303adffcc7bf55ba69b0e0c2bb1985
danielbui78 <danielbui78@gmail.com>
2023-06-27 11:17:00 -0700

    Materials Blend Mode Alpha Fix

--- Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/__init__.py
+++ Blender/appdata_common/Blender Foundation/Blender/BLENDER_VERSION/scripts/addons/DTB/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "DazToBlender",
     "author": "Daz 3D | https://www.daz3d.com",
-    "version": (2022, 2, 19, 43),
+    "version": (2023, 1, 0, 4),
     "blender": (2, 80, 0),
     "location": "3DView > ToolShelf",
     "description": "Daz 3D Genesis 3/8 transfer to Blender",
