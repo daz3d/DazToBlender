edc7e72234a10ca74a9b3eb1500a4231223c3cc6
danielbui78 <danielbui78@gmail.com>
2024-08-14 14:48:35 -0400

    Resync with merged and updated 2024 bridge library

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -3,8 +3,8 @@
 
 // Version number for dzBridge-Blender
 #define PLUGIN_MAJOR	2024
-#define PLUGIN_MINOR	1
-#define PLUGIN_REV		1
-#define PLUGIN_BUILD	17
+#define PLUGIN_MINOR	2
+#define PLUGIN_REV		0
+#define PLUGIN_BUILD	1
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
